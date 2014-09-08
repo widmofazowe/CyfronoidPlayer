f19aee2843884b16836524b85ae5013f8e8d1c43
widmo <widomski.pawel@gmail.com>
2014-09-08 17:59:30 +0200

    FIX

--- src/eu/cyfronoid/audio/player/component/MusicLibraryTree.java
+++ src/eu/cyfronoid/audio/player/component/MusicLibraryTree.java
@@ -3,22 +3,19 @@ package eu.cyfronoid.audio.player.component;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JComponent;
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.TransferHandler;
+import javax.swing.TransferHandler.TransferSupport;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
-import org.apache.log4j.Logger;
-
+import com.google.common.collect.Lists;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 
@@ -33,16 +30,28 @@ import eu.cyfronoid.gui.tree.TreeState;
 
 public class MusicLibraryTree extends JTree implements TreeSelectionListener {
     private static final long serialVersionUID = 5107297933537534482L;
-    private static final Logger logger = Logger.getLogger(MusicLibraryTree.class);
     private SongTreeModel model;
     private EventBus eventBus = PlayerConfigurator.injector.getInstance(EventBus.class);
     private TreeState treeState;
+    private static final DataFlavor NODES_FLAVOR;
+
+    static {
+        String mimeType = DataFlavor.javaJVMLocalObjectMimeType +
+                ";class=\"" + DefaultMutableTreeNode[].class.getName() +
+                    "\"";
+        try {
+            NODES_FLAVOR = new DataFlavor(mimeType);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public MusicLibraryTree() {
         model = new SongTreeModel(SongLibrary.INSTANCE.buildTree());
         setModel(model);
         //setRootVisible(false);
         setDragEnabled(true);
+        setTransferHandler(new MusicLibraryTreeTransferHandler());
         SwingUtilities.invokeLater(new Runnable() {
 
             @Override
@@ -96,87 +105,43 @@ public class MusicLibraryTree extends JTree implements TreeSelectionListener {
     private static class MusicLibraryTreeTransferHandler extends TransferHandler {
         private static final long serialVersionUID = -7524926382207466058L;
 
-        DataFlavor nodesFlavor;
-        DataFlavor[] flavors = new DataFlavor[1];
-        DefaultMutableTreeNode[] nodesToRemove;
+        private static DataFlavor[] flavors = new DataFlavor[1];
 
-        public MusicLibraryTreeTransferHandler() {
-            try {
-                String mimeType = DataFlavor.javaJVMLocalObjectMimeType +
-                                  ";class=\"" +
-                    javax.swing.tree.DefaultMutableTreeNode[].class.getName() +
-                                  "\"";
-                nodesFlavor = new DataFlavor(mimeType);
-                flavors[0] = nodesFlavor;
-            } catch(ClassNotFoundException e) {
-                System.out.println("ClassNotFound: " + e.getMessage());
-            }
+        static {
+            flavors[0] = NODES_FLAVOR;
         }
 
-        public boolean canImport(TransferHandler.TransferSupport support) {
-            return false;
+        @Override
+        public boolean canImport(TransferHandler.TransferSupport info) {
+            return info.isDataFlavorSupported(NODES_FLAVOR);
         }
 
+        @Override
         protected Transferable createTransferable(JComponent c) {
             JTree tree = (JTree)c;
             TreePath[] paths = tree.getSelectionPaths();
             if(paths != null) {
-                // Make up a node array of copies for transfer and
-                // another for/of the nodes that will be removed in
-                // exportDone after a successful drop.
-                List<DefaultMutableTreeNode> copies =
-                    new ArrayList<DefaultMutableTreeNode>();
-                List<DefaultMutableTreeNode> toRemove =
-                    new ArrayList<DefaultMutableTreeNode>();
-                DefaultMutableTreeNode node =
-                    (DefaultMutableTreeNode)paths[0].getLastPathComponent();
-                DefaultMutableTreeNode copy = copy(node);
-                copies.add(copy);
-                toRemove.add(node);
+                List<DefaultMutableTreeNode> selected = Lists.newArrayList();
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) paths[0].getLastPathComponent();
+                selected.add(node);
                 for(int i = 1; i < paths.length; i++) {
-                    DefaultMutableTreeNode next =
-                        (DefaultMutableTreeNode)paths[i].getLastPathComponent();
-                    // Do not allow higher level nodes to be added to list.
-                    if(next.getLevel() < node.getLevel()) {
-                        break;
-                    } else if(next.getLevel() > node.getLevel()) {  // child node
-                        copy.add(copy(next));
-                        // node already contains child
-                    } else {                                        // sibling
-                        copies.add(copy(next));
-                        toRemove.add(next);
-                    }
+                    DefaultMutableTreeNode next = (DefaultMutableTreeNode) paths[i].getLastPathComponent();
+                    selected.add(next);
                 }
-                DefaultMutableTreeNode[] nodes =
-                    copies.toArray(new DefaultMutableTreeNode[copies.size()]);
-                nodesToRemove =
-                    toRemove.toArray(new DefaultMutableTreeNode[toRemove.size()]);
+                DefaultMutableTreeNode[] nodes = selected.toArray(new DefaultMutableTreeNode[selected.size()]);
                 return new NodesTransferable(nodes);
             }
             return null;
         }
 
-        private DefaultMutableTreeNode copy(TreeNode node) {
-            return new DefaultMutableTreeNode(node);
-        }
-
-        protected void exportDone(JComponent source, Transferable data, int action) {
-            if((action & MOVE) == MOVE) {
-                JTree tree = (JTree)source;
-                DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
-                // Remove nodes saved in nodesToRemove in createTransferable.
-                for(int i = 0; i < nodesToRemove.length; i++) {
-                    model.removeNodeFromParent(nodesToRemove[i]);
-                }
+        @Override
+        public boolean importData(TransferHandler.TransferSupport info) {
+            if (!info.isDrop()) {
+                return false;
             }
-        }
 
-        public int getSourceActions(JComponent c) {
-            return COPY_OR_MOVE;
-        }
-
-        public String toString() {
-            return getClass().getName();
+            System.out.println("as");
+            return true;
         }
 
         public class NodesTransferable implements Transferable {
@@ -184,8 +149,9 @@ public class MusicLibraryTree extends JTree implements TreeSelectionListener {
 
             public NodesTransferable(DefaultMutableTreeNode[] nodes) {
                 this.nodes = nodes;
-             }
+            }
 
+            @Override
             public Object getTransferData(DataFlavor flavor)
                                      throws UnsupportedFlavorException {
                 if(!isDataFlavorSupported(flavor))
@@ -193,12 +159,14 @@ public class MusicLibraryTree extends JTree implements TreeSelectionListener {
                 return nodes;
             }
 
+            @Override
             public DataFlavor[] getTransferDataFlavors() {
                 return flavors;
             }
 
+            @Override
             public boolean isDataFlavorSupported(DataFlavor flavor) {
-                return nodesFlavor.equals(flavor);
+                return NODES_FLAVOR.equals(flavor);
             }
         }
     }
