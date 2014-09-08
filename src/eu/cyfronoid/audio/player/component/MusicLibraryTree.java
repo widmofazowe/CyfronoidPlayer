c8dbc178fb95bdd1d72782eda7d8c820d5319b27
widmo <widomski.pawel@gmail.com>
2014-09-08 17:06:32 +0200

    ADMIN # transferable

--- src/eu/cyfronoid/audio/player/component/MusicLibraryTree.java
+++ src/eu/cyfronoid/audio/player/component/MusicLibraryTree.java
@@ -1,10 +1,21 @@
 package eu.cyfronoid.audio.player.component;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComponent;
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
+import javax.swing.TransferHandler;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 import org.apache.log4j.Logger;
 
@@ -82,4 +93,113 @@ public class MusicLibraryTree extends JTree implements TreeSelectionListener {
         });
     }
 
+    private static class MusicLibraryTreeTransferHandler extends TransferHandler {
+        private static final long serialVersionUID = -7524926382207466058L;
+
+        DataFlavor nodesFlavor;
+        DataFlavor[] flavors = new DataFlavor[1];
+        DefaultMutableTreeNode[] nodesToRemove;
+
+        public MusicLibraryTreeTransferHandler() {
+            try {
+                String mimeType = DataFlavor.javaJVMLocalObjectMimeType +
+                                  ";class=\"" +
+                    javax.swing.tree.DefaultMutableTreeNode[].class.getName() +
+                                  "\"";
+                nodesFlavor = new DataFlavor(mimeType);
+                flavors[0] = nodesFlavor;
+            } catch(ClassNotFoundException e) {
+                System.out.println("ClassNotFound: " + e.getMessage());
+            }
+        }
+
+        public boolean canImport(TransferHandler.TransferSupport support) {
+            return false;
+        }
+
+        protected Transferable createTransferable(JComponent c) {
+            JTree tree = (JTree)c;
+            TreePath[] paths = tree.getSelectionPaths();
+            if(paths != null) {
+                // Make up a node array of copies for transfer and
+                // another for/of the nodes that will be removed in
+                // exportDone after a successful drop.
+                List<DefaultMutableTreeNode> copies =
+                    new ArrayList<DefaultMutableTreeNode>();
+                List<DefaultMutableTreeNode> toRemove =
+                    new ArrayList<DefaultMutableTreeNode>();
+                DefaultMutableTreeNode node =
+                    (DefaultMutableTreeNode)paths[0].getLastPathComponent();
+                DefaultMutableTreeNode copy = copy(node);
+                copies.add(copy);
+                toRemove.add(node);
+                for(int i = 1; i < paths.length; i++) {
+                    DefaultMutableTreeNode next =
+                        (DefaultMutableTreeNode)paths[i].getLastPathComponent();
+                    // Do not allow higher level nodes to be added to list.
+                    if(next.getLevel() < node.getLevel()) {
+                        break;
+                    } else if(next.getLevel() > node.getLevel()) {  // child node
+                        copy.add(copy(next));
+                        // node already contains child
+                    } else {                                        // sibling
+                        copies.add(copy(next));
+                        toRemove.add(next);
+                    }
+                }
+                DefaultMutableTreeNode[] nodes =
+                    copies.toArray(new DefaultMutableTreeNode[copies.size()]);
+                nodesToRemove =
+                    toRemove.toArray(new DefaultMutableTreeNode[toRemove.size()]);
+                return new NodesTransferable(nodes);
+            }
+            return null;
+        }
+
+        private DefaultMutableTreeNode copy(TreeNode node) {
+            return new DefaultMutableTreeNode(node);
+        }
+
+        protected void exportDone(JComponent source, Transferable data, int action) {
+            if((action & MOVE) == MOVE) {
+                JTree tree = (JTree)source;
+                DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
+                // Remove nodes saved in nodesToRemove in createTransferable.
+                for(int i = 0; i < nodesToRemove.length; i++) {
+                    model.removeNodeFromParent(nodesToRemove[i]);
+                }
+            }
+        }
+
+        public int getSourceActions(JComponent c) {
+            return COPY_OR_MOVE;
+        }
+
+        public String toString() {
+            return getClass().getName();
+        }
+
+        public class NodesTransferable implements Transferable {
+            DefaultMutableTreeNode[] nodes;
+
+            public NodesTransferable(DefaultMutableTreeNode[] nodes) {
+                this.nodes = nodes;
+             }
+
+            public Object getTransferData(DataFlavor flavor)
+                                     throws UnsupportedFlavorException {
+                if(!isDataFlavorSupported(flavor))
+                    throw new UnsupportedFlavorException(flavor);
+                return nodes;
+            }
+
+            public DataFlavor[] getTransferDataFlavors() {
+                return flavors;
+            }
+
+            public boolean isDataFlavorSupported(DataFlavor flavor) {
+                return nodesFlavor.equals(flavor);
+            }
+        }
+    }
 }
